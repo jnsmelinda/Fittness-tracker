e29dbb9aac10fa4a3247f5c2431a84d55510762e
Melinda Ivanov <jns.melinda@gmail.com>
2020-10-13 23:12:24 -0700

    adding stats

--- server.js
+++ server.js
@@ -44,5 +44,6 @@ function enhanceWithTotalDuration(workout) {
 }
 
 app.get("/exercise", (req, res) => res.sendFile(path.join(__dirname, "public/exercise.html")));
+app.get("/stats", (req, res) => res.sendFile(path.join(__dirname, "public/stats.html")));
 
 app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
